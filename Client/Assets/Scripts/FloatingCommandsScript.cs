391980dbec6d59379a4282794dcc9eb18d2a3b84
Seoyoun <zzzzqqqw@naver.com>
2024-10-08 21:32:25 +0900

    Feat : interactive UI
    - 버튼 컴포넌트 찾고 클릭 시마다 .interactable t/f
    - 버튼 클릭 시 delete(InputTextField.Text)
    - 명령어 텍스트가 떠다니는 인터랙티브한 background ui 구현

---
+++ Client/Assets/Scripts/FloatingCommandsScript.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveCommands : MonoBehaviour
+{
+    public GameObject FloatingTextPrefab;
+    public Vector2 boundarySize;
+    public List<string> commandList = new List<string>();
+
+    void Start()
+    {
+        commandList.Add("음악 생성");
+        commandList.Add("언어 학습");
+        commandList.Add("IoT 설정");
+        commandList.Add("인터랙티브한 명령어 4");
+        commandList.Add("인터랙티브한 명령어  5");
+
+        foreach (var commandText in commandList)
+        {
+            CreateFloatingCommand(commandText);
+        }
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            MoveFloatingCommand(transform.GetChild(i).gameObject);
+        }
+    }
+
+    void CreateFloatingCommand(string commandText)
+    {
+        boundarySize = new Vector2(1720, 880);  // Canvas size -200
+
+        Vector3 startPosition = new Vector3(
+            Random.Range(-boundarySize.x / 2, boundarySize.x / 2),
+            Random.Range(-boundarySize.y / 2, boundarySize.y / 2),
+            0
+        );
+
+        var newCommand = Instantiate(FloatingTextPrefab, transform);
+
+        RectTransform rectTransform = newCommand.GetComponent<RectTransform>();
+        TextMesh textMesh = newCommand.GetComponent<TextMesh>();
+
+        rectTransform.anchoredPosition = startPosition;
+        newCommand.GetComponent<TextMesh>().text = commandText; 
+        //float randomLocalScale = Random.Range(10, 30);
+        
+        //rectTransform.localScale = new Vector3(randomLocalScale, randomLocalScale, randomLocalScale);
+        textMesh.characterSize = Random.Range(20, 50);
+
+        newCommand.AddComponent<CommandMover>().Initialize(boundarySize);
+    }
+
+    // 명령어 이동 처리 함수
+    void MoveFloatingCommand(GameObject command)
+    {
+        CommandMover mover = command.GetComponent<CommandMover>();
+        if (mover != null)
+        {
+            mover.Move();
+        }
+    }
+}
+
+// 개별 명령어 이동 및 충돌 처리 클래스
+public class CommandMover : MonoBehaviour
+{
+    private Vector3 direction;
+    private float speed = 20f;
+    private Vector2 boundary;
+    private RectTransform rectTransform;
+
+    public void Initialize(Vector2 boundarySize)
+    {
+        boundary = boundarySize;
+        rectTransform = GetComponent<RectTransform>();
+        direction = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0).normalized;
+    }
+
+    // 이동 처리 함수
+    public void Move()
+    {
+        rectTransform.anchoredPosition += (Vector2)(direction * speed * Time.deltaTime);
+        CheckBoundaryCollision();
+    }
+
+    // 경계 충돌 반응
+    private void CheckBoundaryCollision()
+    {
+        Vector3 pos = rectTransform.anchoredPosition;
+
+        // 1: ui 화면 경계에 충돌할 경우 xy축 반전.
+        if (pos.x < -boundary.x / 2 || pos.x > boundary.x / 2)
+        {
+            direction.x = -direction.x;  // x축 방향 반전
+            pos.x = Mathf.Clamp(pos.x, -boundary.x / 2, boundary.x / 2);
+        }
+
+        if (pos.y < -boundary.y / 2 || pos.y > boundary.y / 2)
+        {
+            direction.y = -direction.y;  // y축 방향 반전
+            pos.y = Mathf.Clamp(pos.y, -boundary.y / 2, boundary.y / 2);
+        }
+
+        rectTransform.anchoredPosition = pos;
+
+        // 2: 각 명령어에 충돌할 경우 xy축 반전. 구현x
+
+    }
+}
