23d4ccb441b557c7feabff1cb9ae30574eba3439
Seoyoun <zzzzqqqw@naver.com>
2024-10-10 20:41:48 +0900

    Feat : Character Scale by EXP
    + builtin setting

--- Client/Assets/Scripts/InputCommands.cs
+++ Client/Assets/Scripts/InputCommands.cs
@@ -1,11 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine.UI;
-using System.Linq;
-using UniRx;
-using UniRx.Triggers;
 using UnityEngine;
-using System;
 using TMPro;
 
 public class InputCommands : MonoBehaviour
@@ -13,6 +7,7 @@ public class InputCommands : MonoBehaviour
     public Button button { get; set; }
     public TMP_InputField text { get; set; }
     public string inputText;
+    public float exp;
 
     public void changeButton()
     {
@@ -23,10 +18,12 @@ public class InputCommands : MonoBehaviour
     {
         button = GameObject.FindGameObjectWithTag("InputCommandButton").GetComponent<Button>();
         text = GameObject.FindGameObjectWithTag("InputCommandText").GetComponent<TMP_InputField>();
+        exp = 0;
     }
 
     public void SendCommand()
     {
+        exp += 1;
         Debug.Log($"입력된 명령어: {text.text}");
         text.text = "";
         button.interactable = !button.interactable;
